10ba484301b162a63c3e3325bc8c62abf2a10df2
David Ha <David.ha0908@gmail.com>
2021-10-21 08:48:12 -0600

    fixed conventions

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ const unsplashPictures = [
     description: 'picture of a lego stormtrooper is casually walking on sand',
     width: 600,
     height: 300,
-    pathURL: 'images/cPF2nlWcMY4.jpg',
+    pathURL: 'assets/images/cPF2nlWcMY4.jpg',
     linkURL: 'https://unsplash.com/photos/cPF2nlWcMY4',
     credit: 'Daniel Cheung',
     creditURL: 'https://unsplash.com/@danielkcheung'
@@ -17,7 +17,7 @@ const unsplashPictures = [
     description: 'picture of a lego stormtrooper that is holding a paint brush and ready to paint',
     width: 600,
     height: 300,
-    pathURL: 'images/ZqqlOZyGG7g.jpg',
+    pathURL: 'assets/images/ZqqlOZyGG7g.jpg',
     linkURL: 'https://unsplash.com/photos/ZqqlOZyGG7g',
     credit: 'Daniel Cheung',
     creditURL: 'https://unsplash.com/@danielkcheung'
@@ -29,7 +29,7 @@ const unsplashPictures = [
     description: 'picture of a lego micky mouse hanging with his friends infront of a castle',
     width: 600,
     height: 300,
-    pathURL: 'images/ml2F5o0nLh8.jpg',
+    pathURL: 'assets/images/ml2F5o0nLh8.jpg',
     linkURL: 'https://unsplash.com/photos/ml2F5o0nLh8',
     credit: 'Rod Long',
     creditURL: 'https://unsplash.com/@rodlong'
@@ -41,7 +41,7 @@ const unsplashPictures = [
     description: 'picture of a lego man dressed in a firework costume and holding a balloon',
     width: 600,
     height: 300,
-    pathURL: 'images/8yCmQODY2SY.jpg',
+    pathURL: 'assets/images/8yCmQODY2SY.jpg',
     linkURL: 'https://unsplash.com/photos/8yCmQODY2SY',
     credit: 'Hello Im Nik',
     creditURL: 'https://unsplash.com/@helloimnik'
@@ -53,7 +53,7 @@ const unsplashPictures = [
     description: 'picture of a lego Batman on a desk listening to music',
     width: 600,
     height: 300,
-    pathURL: 'images/eTTlZWec6fw.jpg',
+    pathURL: 'assets/images/eTTlZWec6fw.jpg',
     linkURL: 'https://unsplash.com/photos/eTTlZWec6fw',
     credit: 'Viktor Bystrov',
     creditURL: 'https://unsplash.com/@xokvictor'
@@ -65,7 +65,7 @@ const unsplashPictures = [
     description: 'picture of a someone holding a lego man looking over a beautiful bridge with a view',
     width: 600,
     height: 300,
-    pathURL: 'images/QfOBbZcHZBw.jpg',
+    pathURL: 'assets/images/QfOBbZcHZBw.jpg',
     linkURL: 'https://unsplash.com/photos/QfOBbZcHZBw',
     credit: 'Henry',
     creditURL: 'https://unsplash.com/@hurnyh'
@@ -77,7 +77,7 @@ const unsplashPictures = [
     description: 'picture of a lego stormtrooper pointing a gun at c3po ',
     width: 600,
     height: 300,
-    pathURL: 'images/42l3tjsJGyw.jpg',
+    pathURL: 'assets/images/42l3tjsJGyw.jpg',
     linkURL: 'https://unsplash.com/photos/42l3tjsJGyw',
     credit: 'Artur Tumasjan',
     creditURL: 'https://unsplash.com/@arturtumasjan'
@@ -89,7 +89,7 @@ const unsplashPictures = [
     description: 'picture of a lego groot exploring the wilderness',
     width: 600,
     height: 300,
-    pathURL: 'images/zfbAvm3ocz4.jpg',
+    pathURL: 'assets/images/zfbAvm3ocz4.jpg',
     linkURL: 'https://unsplash.com/photos/zfbAvm3ocz4',
     credit: 'Sid Balachandran',
     creditURL: 'https://unsplash.com/@itookthose'
@@ -101,7 +101,7 @@ const unsplashPictures = [
     description: 'picture of a lego stormtrooper walking in the snow on a moonlit night',
     width: 600,
     height: 300,
-    pathURL: 'images/G3eUb_ilKj0.jpg',
+    pathURL: 'assets/images/G3eUb_ilKj0.jpg',
     linkURL: 'https://unsplash.com/photos/G3eUb_ilKj0',
     credit: 'Umberto',
     creditURL: 'https://unsplash.com/@umby'
@@ -113,7 +113,7 @@ const unsplashPictures = [
     description: 'picture of a lego stormtrooper and han solo holding up yoda',
     width: 600,
     height: 300,
-    pathURL: 'images/lBCpc87rtKo.jpg',
+    pathURL: 'assets/images/lBCpc87rtKo.jpg',
     linkURL: 'https://unsplash.com/photos/lBCpc87rtKo',
     credit: 'Artur Tumasjan',
     creditURL: 'https://unsplash.com/@arturtumasjan'
@@ -125,17 +125,18 @@ const gallery = document.querySelector('.gallery')
 
 unsplashPictures.forEach(function(picture){
   output += `
-  <figure>
-    <h2 class="title">${picture.title}</h2>
-    <a href="${picture.linkURL}">
-      <img src="${picture.pathURL}" alt="${picture.description}" width="${picture.width}" height="${picture.height}">
-    </a>
-  <figcaption>
-    <a class="author" href="${picture.creditURL}">
-      <span>${picture.credit}</span>
-    </a>
-  </figcaption>
-  </figure>`
+    <figure>
+      <h2 class="title">${picture.title}</h2>
+      <a href="${picture.linkURL}">
+        <img src="${picture.pathURL}" alt="${picture.description}" width="${picture.width}" height="${picture.height}">
+      </a>
+      <figcaption>
+        <a class="author" href="${picture.creditURL}">
+          <span>${picture.credit}</span>
+        </a>
+      </figcaption>
+    </figure>
+  `
 })
 
 gallery.innerHTML = output;
